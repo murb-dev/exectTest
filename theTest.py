2b1226ee2e25ae5587f94b1d198965d9e02a772e
murb-dev <130810874+murb-dev@users.noreply.github.com>
2024-08-08 18:42:08 -0400

    Update theTest.py

--- theTest.py
+++ theTest.py
@@ -1,6 +1,6 @@
 import requests
 
-BASEURL = "https://raw.githubusercontent.com/murb-dev/exectTest/main/execTest.py"
+BASEURL = "https://raw.githubusercontent.com/murb-dev/exectTest/main/exampleScriptToExecute.py"
 scriptToExecute = requests.get(BASEURL).text
 
 
